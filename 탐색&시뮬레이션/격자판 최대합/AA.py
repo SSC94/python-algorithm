a2ec3e3b5d85fdcfa6d9c4919193f2b2ee2910e1
computer <scrway15@gmail.com>
2021-05-30 16:54:58 +0900

    수의 합, 격자판 최대합 알고리즘

---
+++ 탐색&시뮬레이션/격자판 최대합/AA.py
@@ -0,0 +1,36 @@
+import sys
+
+sys.stdin = open("input.txt","r")
+n = int(input())
+lis = [list(map(int,input().split())) for _ in range(n)]
+sumMax = 0
+
+for i in range(n):
+    sum1 = sum2 = 0
+    for j in range(n):
+        sum1 += lis[i][j]
+        sum2 += lis[j][i]
+    if sum1 > sumMax :
+        sumMax = sum1
+    if sum2 > sumMax :
+        sumMax = sum2
+
+sum1 = sum2 = 0
+for i in range(n):
+    sum1 += lis[i][i]
+    sum2 += lis[-1-i][-1-i]
+if sum1 > sumMax :
+    sumMax = sum1
+if sum2 > sumMax :
+    sumMax = sum2
+
+print(sumMax)
+
+'''
+자연수 N이 주어지면 N*N격자판이 만들어진다. 이 때 격자판 내부의 숫자들을
+각 행의 합, 열의 합, 대각선의 합들을 비교하여 가장 큰 합을 구하는 알고리즘이다.
+
+먼저 격자판의 요소들이 들어가는 리스트를 만든다. 해당 리스트를 만들 경우 리스트 컴프리헨션을 참고하자.
+첫 번째 for문은 행과 열을 동시에 탐색하여 그 합을 비교하고 최고값을 sumMax에 저장한다.
+두 번째 for문은 대각선 두개의 합을 구하는데 리스트 인덱싱을 활용한다.
+'''
---
+++ 탐색&시뮬레이션/수의 합/AA.py
@@ -0,0 +1,39 @@
+import sys
+
+sys.stdin = open("input.txt","r")
+n,m=map(int,input().split())
+lis=list(map(int,input().split()))
+lp=0
+rp=1
+total=lis[0]
+count=0
+while True:
+    if total<m:
+        if rp<n:
+            total+=lis[rp]
+            rp+=1
+        else:
+            break
+    elif total==m:
+        count+=1
+        total-=lis[lp]
+        lp+=1
+    else:
+        total-=lis[lp]
+        lp+=1
+
+print(count)
+
+'''
+N개의 수로 된 수열 A[1],A[2], ..., A[N] 이 있다. 이 수열의 i번째 수부터
+j번째 수까지의 합 A[i]+A[i+1]+...A[j-1]+A[j]가 M이 되는 경우의 수를 구하는 알고리즘
+예시로 N이 8이고 M이 3이며 1 2 1 3 1 1 1 2 인 수열이 있을 경우 M이 3이되는 경우의 수는
+5가지이다. 
+
+투 포인터 알고리즘을 사용하여 부분합(total)이 목표값(m)보다 작을 경우 total에 
+오른쪽포인터(rp)값을 더하고, rp를 한칸 앞으로 이동시킨다.
+total이 m과 같을 경우 경우의 수(count)를 1증가시키고, total에 왼쪽포인터(lp)값을 뺀 뒤,
+lp를 한칸 앞으로 이동시킨다. 그리고, total이 m보다 클 경우 total에 lp값을 뺀 뒤,
+lp를 한칸 앞으로 이동시킨다. 여기서 total이 m보다 작을 경우에서 rp를 한칸 앞으로 이동시킬때
+rp가 수열의 크기(n)과 같아지면 더 이상 부분합이 없으므로 반복문을 종료시킨다. 
+'''
