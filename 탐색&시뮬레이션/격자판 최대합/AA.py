ed0eae0209aac055791b2e0bb9bbee8609be9a85
SSC94 <68208144+SSC94@users.noreply.github.com>
2021-06-15 23:57:39 +0900

    추가 코드 작성

--- 탐색&시뮬레이션/격자판 최대합/AA.py
+++ 탐색&시뮬레이션/격자판 최대합/AA.py
@@ -26,6 +26,25 @@ if sum2 > sumMax :
 
 print(sumMax)
 
+# 추가 코드
+# import sys
+# sys.stdin = open('input.txt','r')
+# n = int(sys.stdin.readline())
+# lis = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
+# max_num = sum_dia1 = sum_dia2 = 0
+# for i in range(n):
+#     sum_row = sum_col = 0
+#     for j in range(n):
+#         sum_row += lis[i][j]
+#         sum_col += lis[j][i]
+#     max_num = max(max_num,sum_row,sum_col)
+# for i in range(n):
+#     sum_dia1 += lis[i][i]
+#     sum_dia2 += lis[-i-1][-i-1]
+# max_num = max(max_num,sum_dia1,sum_dia2)
+# print(max_num)
+
+
 '''
 자연수 N이 주어지면 N*N격자판이 만들어진다. 이 때 격자판 내부의 숫자들을
 각 행의 합, 열의 합, 대각선의 합들을 비교하여 가장 큰 합을 구하는 알고리즘이다.
